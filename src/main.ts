7cb5583717111bc1728f1719e594f340e7a355f5
DanielBarros854 <danielbarros854@yahoo.com>
2025-04-23 15:41:28 -0300

    feat: config cors

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [`${process.env.FRONTEND_DOMAIN}`, 'http://localhost:3000'],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT);
   console.log(
     `Server is running, GraphQL Playground available at http://localhost:${process.env.PORT}/graphql`,
   );
 }
+
 bootstrap();
